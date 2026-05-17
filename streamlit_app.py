073a2dc5c8aea5973ac3cf3c24d50e5f495b498d
Gandesiri Shravan Kumar <162951704+Shravank9@users.noreply.github.com>
2026-05-17 18:44:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
 
     st.subheader("Dataset Preview")
-    st.dataframe(df)
+    st.dataframe(df.drop("target", axis=1))
 
     st.subheader("Dataset Preprocessing")
 
